94fe56082a4d815b61f64f11d9325c376b400871
Abddev09 <abddev09@gmail.com>
2025-08-25 08:29:21 +0500

    task 1 added test and views api post path add

--- card/views.py
+++ card/views.py
@@ -1,23 +1,27 @@
-import pandas as pd
-from rest_framework import generics, parsers, status
+from rest_framework import viewsets, parsers, status
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from card.models import Card
+from card.serializers import CardSerializer
 from card.utils.utils import format_card_number, format_phone_number, format_expire, format_balance
 
 
-class ImportCardData(generics.GenericAPIView):
-    parser_classes = [parsers.MultiPartParser]
+class CardViewSet(viewsets.ViewSet):
+    parser_classes = [parsers.MultiPartParser,parsers.JSONParser, parsers.FormParser]
 
-    def post(self, request, *args, **kwargs):
+    @action(detail=False, methods=['post'], url_path='import')
+    def import_cards(self, request):
         file_obj = request.FILES.get("file")
         if not file_obj:
             return Response({"error": "File not provided"}, status=status.HTTP_400_BAD_REQUEST)
 
         try:
+            import pandas as pd
             if file_obj.name.endswith(".csv"):
                 df = pd.read_csv(file_obj)
             else:
-                df = pd.read_excel(file_obj)
+                df = pd.read_excel(file_obj, engine='openpyxl')  # <--- shu yerga engine qo‘shish
+
         except Exception as e:
             return Response({"error": f"Failed to read file: {str(e)}"}, status=status.HTTP_400_BAD_REQUEST)
 
@@ -30,7 +34,7 @@ class ImportCardData(generics.GenericAPIView):
             balance = format_balance(row.get("balance"))
 
             if not card_number:
-                continue  # agar kartaning raqami yo‘q bo‘lsa, tashlab ketamiz
+                continue
 
             card, created = Card.objects.update_or_create(
                 card_number=card_number,
@@ -51,3 +55,11 @@ class ImportCardData(generics.GenericAPIView):
             "created": created_count,
             "updated": updated_count
         }, status=status.HTTP_200_OK)
+
+    @action(detail=False, methods=['post'], url_path='add')
+    def add_card(self, request):
+        serializer = CardSerializer(data=request.data)
+        if serializer.is_valid():
+            card = serializer.save()
+            return Response({"message": "Card added", "card": CardSerializer(card).data})
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
